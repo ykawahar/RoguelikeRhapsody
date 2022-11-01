b00299f14e4c0e201e03758b100b795a157f9fb6
Shalu Patel <shalu.patel1217@gmail.com>
2022-11-01 11:03:30 -0500

    Created Placeholder Map Scene
    Created a map scene with placeholder button nodes
    
    need to create clickable

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    //start screen leads into map nodes (show all possible options?)
+    //make all nodes past current column not interactable (interactable nodes should scale bigger or highlight)
+    //when valid node is clicked on, (generate mode method) which fires the respective script for generating the parameters of the node
+    //i.e. level manager creates the fight scene --- tribute manager creates the tribute scene
+    
+    //todo: 
+    //create placeholder scenes --- for each type of scenario
+    //the generateMode method --- which actually place the values/options into the placeholder assets
+    //for example; there will be two torch icons with placeholder text underneath, the generateTribute scene method should edit the text
+    //from a range of options (weighted list/array) that replaces the placeholder
+    //list of parameters to weight --- tribute buffs/stats/weapons
+    // 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
